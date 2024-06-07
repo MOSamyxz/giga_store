a89fa5755088e4884d71903ed5308b1941ca5be1
Muhammad Samy <moosamyz9@gmail.com>
2024-06-07 16:24:01 +0300

    starter project

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,40 @@
+class AppAssets {
+  static const String iconsPath = 'assets/icons';
+
+  // Icons
+  static const String back = '$iconsPath/Back.png';
+  static const String backWhite = '$iconsPath/Back-white.png';
+  static const String bell = '$iconsPath/Bell.png';
+  static const String book = '$iconsPath/Book.png';
+  static const String box = '$iconsPath/Box.png';
+  static const String buyWhite = '$iconsPath/Buy-white.png';
+  static const String cart = '$iconsPath/Cart.png';
+  static const String checkColor = '$iconsPath/Check-color.png';
+  static const String check = '$iconsPath/Check.png';
+  static const String close = '$iconsPath/Close.png';
+  static const String closeWhite = '$iconsPath/Close-white.png';
+  static const String faq = '$iconsPath/FAQ.png';
+  static const String faqColor = '$iconsPath/FAQ-color.png';
+  static const String filter = '$iconsPath/FAQ-color.png';
+  static const String fish = '$iconsPath/Fish.png';
+  static const String grid = '$iconsPath/Grid.png';
+  static const String headphone = '$iconsPath/Heqadphone.png';
+  static const String heart = '$iconsPath/heart.png';
+  static const String homeColor = '$iconsPath/Home-color.png';
+  static const String home = '$iconsPath/Home.png';
+  static const String list = '$iconsPath/List.png';
+  static const String menu = '$iconsPath/menu.png';
+  static const String monitor = '$iconsPath/Monitor.png';
+  static const String orders = '$iconsPath/Orders.png';
+  static const String phone = '$iconsPath/Phone.png';
+  static const String print = '$iconsPath/Print.png';
+  static const String searchColor = '$iconsPath/Search-color.png';
+  static const String search = '$iconsPath/Search.png';
+  static const String share = '$iconsPath/Share.png';
+  static const String shoe = '$iconsPath/Shoe.png';
+  static const String starFilled = '$iconsPath/Star-filled.png';
+  static const String star = '$iconsPath/Star.png';
+  static const String tShirt = '$iconsPath/T-shirt.png';
+  static const String wallet = '$iconsPath/Wallet.png';
+
+}
