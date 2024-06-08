4f78d255ebaf47345e76b0398c2ee9083f3f69c1
Muhammad Samy <moosamyz9@gmail.com>
2024-06-08 17:33:58 +0300

    Starter project

---
+++ lib/core/constants/strings.dart
@@ -0,0 +1,6 @@
+class AppStrings {
+  // onBoarding
+  static const String onBoardingOne = 'Choose The Products';
+  static const String onBoardingTwo = '';
+  static const String onBoardingThree = '';
+}
